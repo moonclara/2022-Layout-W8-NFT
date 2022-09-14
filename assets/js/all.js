80cc11192f3ba1da82b51f580f97e60e5b343ebc
Clara <smile629639@gmail.com>
2022-09-14 16:00:09 +0800

    Update 2022-09-14T08:00:02.075Z

---
+++ assets/js/all.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5'); //swiper
+
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    // autoplay: 3000,//可选选项，自动滑动
+    centeredSlides: true,
+    slidesPerView: 3,
+    spaceBetween: 24,
+    pagination: '.swiper-pagination'
+  });
+}); // vanilla JS
+// init with element
+
+var grid = document.querySelector('.grid'); // init with selector
+
+var msnry = new Masonry('.grid', {// options...
+});
+//# sourceMappingURL=all.js.map
